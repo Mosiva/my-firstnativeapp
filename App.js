44fd57f2bf319d94bb17e4b552b0f95a6475de7a
Mosiva <portret008@gmail.com>
2024-02-29 16:12:45 +0600

    add logic of input text and add items and map it

--- App.js
+++ App.js
@@ -1,14 +1,34 @@
+import { useRef, useState } from "react";
 import { Button, StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function App() {
+  const [enteredGoalText, setEneteredGoalText] = useState("");
+  const [courseGoals, setCourseGoals] = useState([]);
+
+  const goalInputHandler = (enteredText) => {
+    setEneteredGoalText(enteredText);
+  };
+
+  const addGoalHandler = () => {
+    setCourseGoals((currentCourseGoals) => [
+      ...currentCourseGoals,
+      enteredGoalText,
+    ]);
+  };
   return (
     <View style={styles.appContainer}>
       <View style={styles.inputContainer}>
-        <TextInput style={styles.textInput} placeholder="Write you thought" />
-        <Button title="Add it" />
+        <TextInput
+          style={styles.textInput}
+          placeholder="Write you thought"
+          onChangeText={goalInputHandler}
+        />
+        <Button title="Add it" onPress={addGoalHandler} />
       </View>
       <View style={styles.goalsContainer}>
-        <Text>Test</Text>
+        {courseGoals.map((goal) => (
+          <Text key={goal}>{goal}</Text>
+        ))}
       </View>
     </View>
   );
