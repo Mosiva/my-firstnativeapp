4b473d9fac190dab3ecbc3e40d5dc101f8f40c25
Mosiva <portret008@gmail.com>
2024-03-02 17:34:35 +0500

    add style effect on press item

--- components/GoalItem.js
+++ components/GoalItem.js
@@ -3,21 +3,27 @@ import { StyleSheet, View, Text, Pressable } from "react-native";
 
 export default function GoalItem({ text, onDeleteItem, id }) {
   return (
-    <Pressable onPress={onDeleteItem.bind(this, id)}>
-      <View style={styles.goalItems}>
+    <View style={styles.goalItems}>
+      <Pressable
+        onPress={onDeleteItem.bind(this, id)}
+        style={({ pressed }) => pressed && styles.pressedItem}
+      >
         <Text style={styles.goalText}>{text}</Text>
-      </View>
-    </Pressable>
+      </Pressable>
+    </View>
   );
 }
 const styles = StyleSheet.create({
   goalItems: {
     margin: 8,
-    padding: 8,
     borderRadius: 6,
     backgroundColor: "purple",
   },
+  pressedItem: {
+    opacity: 0.5,
+  },
   goalText: {
     color: "white",
+    padding: 8,
   },
 });
